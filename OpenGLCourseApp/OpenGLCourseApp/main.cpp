3593d3122f0161ad0356e5205bb8f86630e34365
Michael Hiatt <michael_hiatt@hotmail.com>
2018-10-24 15:50:22 +1100

    Indexed Draws

--- OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -15,7 +15,7 @@ const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.f;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 bool direction = true;
 float triOffset = 0.f;
 float triMaxOffset = 0.7f;
@@ -124,14 +124,26 @@ void CompileShaders()
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
 	GLfloat vertices[] = {
 		-1.f, -1.f, 0.f,
+		0.f, -1.f, 1.f,
 		1.f, -1.f, 0.f,
 		0.f, 1.f, 0.f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -142,6 +154,8 @@ void CreateTriangle()
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 int main()
@@ -193,6 +207,8 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -242,20 +258,20 @@ int main()
 
 		// Clear the window
 		glClearColor(1.f, 1.f, 1.f, 1.f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.f);
 		//model = glm::translate(model, glm::vec3(triOffset, 0.f, 0.f));
-		//model = glm::rotate(model, currentAngle * toRadians, glm::vec3(0.f, 0.f, 1.f));
+		model = glm::rotate(model, currentAngle * toRadians, glm::vec3(0.f, 1.f, 0.f));
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
 		glBindVertexArray(0);
 
