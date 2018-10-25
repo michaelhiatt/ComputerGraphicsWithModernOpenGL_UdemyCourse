4e4dfb54bb149b33c41dcab134007111fa129f17
Michael Hiatt <michael_hiatt@hotmail.com>
2018-10-25 13:59:24 +1100

    Coding: Texture Mapping

--- OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -1,3 +1,5 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <stdio.h>
 #include <string.h>
 #include <cmath>
@@ -10,10 +12,12 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
 #include "Window.h"
 #include "Mesh.h"
 #include "Shader.h"
 #include "Camera.h"
+#include "Texture.h"
 
 
 const float toRadians = 3.14159265f / 180.f;
@@ -24,6 +28,9 @@ std::vector<Mesh *> meshList;
 std::vector<Shader> shaderList;
 
 Camera camera;
+
+Texture brickTexture;
+Texture dirtTexture;
 
 GLfloat deltaTime = 0.f;
 GLfloat lastTime = 0.f;
@@ -52,18 +59,19 @@ void CreateObjects()
 	};
 
 	GLfloat vertices[] = {
-		-1.f, -1.f, 0.f,
-		0.f, -1.f, 1.f,
-		1.f, -1.f, 0.f,
-		0.f, 1.f, 0.f
+		// x	y	z		u	  v
+		-1.f, -1.f, 0.f,	0.f,  0.f,
+		0.f, -1.f, 1.f,		0.5f, 0.f,
+		1.f, -1.f, 0.f,		1.f,  0.f,
+		0.f, 1.f, 0.f,		0.5f, 1.0f
 	};
 
 	Mesh *obj1 = new Mesh();
-	obj1->CreateMesh(vertices, indices, 12, 12);
+	obj1->CreateMesh(vertices, indices, 20, 12);
 	meshList.push_back(obj1);
 
 	Mesh *obj2 = new Mesh();
-	obj2->CreateMesh(vertices, indices, 12, 12);
+	obj2->CreateMesh(vertices, indices, 20, 12);
 	meshList.push_back(obj2);
 }
 
@@ -76,6 +84,12 @@ int main()
 	CreateShaders();
 
 	camera = Camera(glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f), -90.f, 0.f, 5.f, 0.5f);
+
+	brickTexture = Texture("Textures/brick.png");
+	brickTexture.LoadTexture();
+
+	dirtTexture = Texture("Textures/dirt.png");
+	dirtTexture.LoadTexture();
 
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0;
 	glm::mat4 projection(1.f);
@@ -113,6 +127,8 @@ int main()
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
 
+		brickTexture.UseTexture();
+
 		meshList[0]->RenderMesh();
 
 		model = glm::mat4(1.f);
@@ -121,6 +137,8 @@ int main()
 		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		
+		dirtTexture.UseTexture();
+
 		meshList[1]->RenderMesh();
 
 		glUseProgram(0);
